c311a4349231cc5b28006aa4dce42b4c086a7f1f
md598 <Mark@duttonracing.com>
2020-07-24 10:52:00 -0500

    analyze changes AGAIN

--- app/server.py
+++ app/server.py
@@ -67,11 +67,25 @@ async def homepage(request):
 async def analyze(request):
   img_data = await request.form()
   img_bytes = await (img_data['file'].read()) ##original
+  img_np = np.array(Image.open(BytesIO(img_bytes)))  
 #  img_bytes = (img_data['file']) 
-  pred = learn.predict(img_bytes)[0]
+#  pred = learn.predict(img_bytes)[0]## original
+  pred = learn.predict(BytesIO(img_bytes))
   return JSONResponse({
-      'results': str(pred)
+#      'results': str(pred) ##original
+       'result': str(pred[0])
   })
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+  img_data = await request.form()
+  img_bytes = await (img_data['file'].read())
+  img_np = np.array(Image.open(BytesIO(img_bytes)))
+  pred = learn.predict(BytesIO(img_bytes))
+  return JSONResponse({
+      'result': str(pred[0])
+  })
+
 
 #@app.route('/analyze', methods=['POST'])
 #async def analyze(request):
