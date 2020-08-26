c9490ea25f38ea23fb3920192dcc0c0b59397940
md598 <Mark@duttonracing.com>
2020-08-26 01:21:56 -0500

    return FileResponse(path_res/'results.csv', media_type='csv') added path_res

--- app/server.py
+++ app/server.py
@@ -81,7 +81,7 @@ async def analyze(request):
   path_res = Path('app/static/')
   df.to_csv(path_res/'results.csv')
 
-  return FileResponse('results.csv', media_type='csv')
+  return FileResponse(path_res/'results.csv', media_type='csv')
 
 
 
