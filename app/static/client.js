d5cc81ed17c2e97c10e74948091107f028a626c8
md598 <Mark@duttonracing.com>
2020-08-25 22:23:25 -0500

    tabular_001

--- app/static/client.js
+++ app/static/client.js
@@ -14,25 +14,53 @@ function showPicked(input) {
   reader.readAsDataURL(input.files[0]);
 }
 
-function analyze() {
-  var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+#Tabular
+function analyze(){
+    var uploadFiles = el('file-input').files;
+    if (uploadFiles.length < 1) alert('Please select 1 file to analyze!');
 
-  el("analyze-button").innerHTML = "Analyzing...";
-  var xhr = new XMLHttpRequest();
-  var loc = window.location;
-  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-    true);
-  xhr.onerror = function() {
-    alert(xhr.responseText);
-  };
-  xhr.onload = function(e) {
-    if (this.readyState === 4) {
-      var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+    el('analyze-button').innerHTML = 'Analyzing...';
+    var xhr = new XMLHttpRequest();
+    var loc = window.location
+    xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+    xhr.onerror = function() {alert (xhr.responseText);}
+    xhr.onload = function(e) {
+        if (this.readyState === 4) {
+          el("result-label").innerHTML = `Result = Good`;
+          
+          download('results.csv', 'results.csv');
+          xhr.send();
+        }
+        el("analyze-button").innerHTML = "Analyze";
+      };
+
+      var fileData = new FormData();
+      fileData.append("file", uploadFiles[0]);
+      xhr.send(fileData);
     }
-    el("analyze-button").innerHTML = "Analyze";
-  };
+  }
+
+
+#Vision
+#function analyze() {
+#  var uploadFiles = el("file-input").files;
+#  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+
+#  el("analyze-button").innerHTML = "Analyzing...";
+#  var xhr = new XMLHttpRequest();
+#  var loc = window.location;
+#  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+#    true);
+#  xhr.onerror = function() {
+#    alert(xhr.responseText);
+#  };
+#  xhr.onload = function(e) {
+#    if (this.readyState === 4) {
+#      var response = JSON.parse(e.target.responseText);
+#      el("result-label").innerHTML = `Result = ${response["result"]}`;
+#    }
+#    el("analyze-button").innerHTML = "Analyze";
+#  };
 
   var fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
