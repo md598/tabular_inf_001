1d320219453a1951293bcc7ecb64e24be1f738b7
md598 <Mark@duttonracing.com>
2020-08-26 01:39:36 -0500

    print df

--- app/server.py
+++ app/server.py
@@ -80,7 +80,9 @@ async def analyze(request):
   # if we want to store the results
   path_res = Path('app/static/')
   df.to_csv(path_res/'results.csv')
-  df.Predictions
+  df.T.head()
+  df.head()
+  print('inference done')
   #return FileResponse(path_res/'results.csv', media_type='csv')
   return FileResponse(f'{path_res}/results.csv', media_type='csv')
 
