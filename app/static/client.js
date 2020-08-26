e6855e5919df4d13377d99220b8f8f07ea244456
md598 <Mark@duttonracing.com>
2020-08-25 23:32:12 -0500

    commented out vision section

--- app/static/client.js
+++ app/static/client.js
@@ -41,29 +41,29 @@ function analyze(){
   }
 
 
-#Vision
-#function analyze() {
-#  var uploadFiles = el("file-input").files;
-#  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+/*Vision
+function analyze() {
+  var uploadFiles = el("file-input").files;
+  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-#  el("analyze-button").innerHTML = "Analyzing...";
-#  var xhr = new XMLHttpRequest();
-#  var loc = window.location;
-#  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-#    true);
-#  xhr.onerror = function() {
-#    alert(xhr.responseText);
-#  };
-#  xhr.onload = function(e) {
-#    if (this.readyState === 4) {
-#      var response = JSON.parse(e.target.responseText);
-#      el("result-label").innerHTML = `Result = ${response["result"]}`;
-#    }
-#    el("analyze-button").innerHTML = "Analyze";
-#  };
+  el("analyze-button").innerHTML = "Analyzing...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]}`;
+    }
+    el("analyze-button").innerHTML = "Analyze";
+  };
 
   var fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
+*/
