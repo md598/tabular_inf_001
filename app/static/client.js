c87997189edc7eb3c413c4d91c79b9b4a78836ad
md598 <Mark@duttonracing.com>
2020-08-26 18:22:57 -0500

    ' strings

--- app/static/client.js
+++ app/static/client.js
@@ -37,16 +37,16 @@ function analyze(){
     el('analyze-button').innerHTML = 'Analyzing...';
     var xhr = new XMLHttpRequest();
     var loc = window.location;
-    xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+    xhr.open("POST", '${loc.protocol}//${loc.hostname}:${loc.port}/analyze', true);
     xhr.onerror = function() {alert (xhr.responseText);}
     xhr.onload = function(e) {
         if (this.readyState === 4) {
           // wanting to send the user the csv file saved in 'analyze' in server.py
           //el("result-label").innerHTML = `Result = File Accepted`;
-          el("result-label").innerHTML = `${loc.protocol}//${loc.hostname}/app/static/results.csv`;
+          el("result-label").innerHTML = '${loc.protocol}//${loc.hostname}/app/static/results.csv';
           //download('results.csv', 'results.csv');
-          download2('results.csv',`${loc.protocol}//${loc.hostname}/app/static/results.csv');
-          el("result-label").innerHTML = `download`;
+          download2('results.csv','${loc.protocol}//${loc.hostname}/app/static/results.csv');
+          el("result-label").innerHTML = 'download';
           xhr.send();
         }
         el("analyze-button").innerHTML = "Analyze";
