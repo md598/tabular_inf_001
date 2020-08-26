70343b3ec1e0b5d911c0295c8592e31bf0a2ccd3
md598 <Mark@duttonracing.com>
2020-08-26 11:24:58 -0500

    console.log("waypoint1"); printing in JS

--- app/static/client.js
+++ app/static/client.js
@@ -2,11 +2,13 @@ var el = x => document.getElementById(x);
 
 function showPicker() {
   el("file-input").click();
+  console.log("waypoint-1");
 }
 
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
+  console.log("waypoint0");
   reader.onload = function(e) {
     el("image-picked").src = e.target.result;
     el("image-picked").className = "";
@@ -22,20 +24,27 @@ function analyze(){
     el('analyze-button').innerHTML = 'Analyzing...';
     var xhr = new XMLHttpRequest();
     var loc = window.location;
+    console.log("Pre-POST"); 
     xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+    console.log("Post-POST"); 
     xhr.onerror = function() {alert (xhr.responseText);}
+    console.log("waypoint1"); 
     xhr.onload = function(e) {
         if (this.readyState === 4) {
           el("result-label").innerHTML = `Result = File Accepted`;
-          
+          console.log("waypoint2");
           download('results.csv', 'results.csv');
+          console.log("waypoint3");
           xhr.send();
         }
         el("analyze-button").innerHTML = "Analyze";
+        console.log("waypoint4");
       };
 
       var fileData = new FormData();
+      console.log("waypoint5");
       fileData.append("file", uploadFiles[0]);
+      console.log("waypoint6");
       xhr.send(fileData);
     }
 
