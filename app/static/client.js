392d01c8727f861806cc10e3d7babf79d8ad242b
md598 <Mark@duttonracing.com>
2020-08-26 14:23:36 -0500

    'results.csv' file append

--- app/static/client.js
+++ app/static/client.js
@@ -2,13 +2,11 @@ var el = x => document.getElementById(x);
 
 function showPicker() {
   el("file-input").click();
-  console.log("waypoint-1");
 }
 
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
-  console.log("waypoint0");
   reader.onload = function(e) {
     el("image-picked").src = e.target.result;
     el("image-picked").className = "";
@@ -20,31 +18,23 @@ function showPicked(input) {
 function analyze(){
     var uploadFiles = el('file-input').files;
     if (uploadFiles.length < 1) alert('Please select 1 file to analyze!');
-
     el('analyze-button').innerHTML = 'Analyzing...';
     var xhr = new XMLHttpRequest();
     var loc = window.location;
-    console.log("Pre-POST"); 
     xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
-    console.log("Post-POST"); 
     xhr.onerror = function() {alert (xhr.responseText);}
-    console.log("waypoint1"); 
     xhr.onload = function(e) {
         if (this.readyState === 4) {
           el("result-label").innerHTML = `Result = File Accepted`;
-          console.log("waypoint2");
           download('results.csv', 'results.csv');
-          console.log("waypoint3");
           xhr.send();
         }
         el("analyze-button").innerHTML = "Analyze";
-        console.log("waypoint4");
       };
 
       var fileData = new FormData();
-      console.log("waypoint5");
-      fileData.append("file", uploadFiles[0]);
-      console.log("waypoint6");
+      //fileData.append("file", uploadFiles[0]); //original
+      fileData.append('results.csv', uploadFiles[0]);
       xhr.send(fileData);
     }
 
